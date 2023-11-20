e609d38bad40f575a19fc186f2e95723bb6a2db8
Fernando Maytorena E <113787130+a01110946@users.noreply.github.com>
2023-11-19 17:30:38 -0800

    Update main.py

--- llm-qa/main.py
+++ llm-qa/main.py
@@ -234,7 +234,7 @@ if prompt := st.chat_input(placeholder=starter_message):
     st.session_state.messages.append(HumanMessage(content=prompt))
     
     # Concatenate history and input
-    # full_input = "\n".join([msg.content for msg in st.session_state.messages] + [prompt])
+    full_input = "\n".join([msg.content for msg in st.session_state.messages] + [prompt])
 
     res_box = st.empty()
     report = []
@@ -261,7 +261,7 @@ if prompt := st.chat_input(placeholder=starter_message):
     # Escape the $ character
     #response_content = response_content.replace("$", "\$")
 
-    st.session_state.messages.append(AIMessage(content=report))
+    st.session_state.messages.append(AIMessage(content=response["output"]))
     st.chat_message("assistant", avatar=img).write(report)
     
     """
