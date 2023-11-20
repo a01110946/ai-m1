465a1109e7bced7398ce9e80e45258b19ebe808c
Fernando Maytorena E <113787130+a01110946@users.noreply.github.com>
2023-11-19 17:33:34 -0800

    Update main.py

--- llm-qa/main.py
+++ llm-qa/main.py
@@ -244,10 +244,10 @@ if prompt := st.chat_input(placeholder=starter_message):
     )
     #response_content = response["output"]
     for resp in response:
-        st.write(resp["output"])  # This will show you the structure of resp in your Streamlit app
+        #st.write(resp["output"])  # This will show you the structure of resp in your Streamlit app
         report.append(resp["output"])
-        #result = "".join(report).strip()
-        #result = result.replace("\n", "")
+        result = "".join(report).strip()
+        result = result.replace("\n", "")
         res_box.markdown('*{report}*')
     
     """
